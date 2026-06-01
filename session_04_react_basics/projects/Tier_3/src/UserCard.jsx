b6e5182d59852efdf0e0e4993c96b86fbd7d9707
Nghia <nd5122004@gmail.com>
2026-06-01 21:26:30 +0700

    Revert "Hoàn thành Tier 4"
    This reverts commit 8e13d3ebe8d13265d4b2ac7eba0b6a549c54fbd1.

--- session_04_react_basics/projects/Tier_3/src/UserCard.jsx
+++
@@ -1,9 +0,0 @@
-function UserCard({ name, email, avatar }) {
-  return (
-    <div className="user-card">
-      <img src={avatar} alt={name} />
-      <h3>{name}</h3>
-      <p>{email}</p>
-    </div>
-  );
-}
